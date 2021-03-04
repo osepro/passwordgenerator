b45e64f6716490c4527cdbf6957860a81d6ad623
Ose Agbadu <osepro@gmail.com>
2021-03-03 18:22:21 -0800

    fix: added password generator

--- src/components/PasswordGenerate.js
+++ src/components/PasswordGenerate.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 function PasswordGenerate({ minimum, maximum }) {
+	function makepassword(length) {
+		let result = "";
+		let characters =
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&#@_-%^!";
+		let charactersLength = characters.length;
+
+		for (var i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
+	}
+
+	const generatedPassword = makepassword(maximum).toUpperCase();
+
 	return (
 		<div>
-			<p>
-				{minimum}
-				{maximum}
-			</p>
+			<p>{generatedPassword}</p>
 		</div>
 	);
 }
